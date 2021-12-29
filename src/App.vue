3c76666d83953210fecfa09fbe411f918a7757a0
Faisal Hanif <faisal01h@gmail.com>
2021-12-29 09:59:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Segoe UI Variable, Segoe UI, Roboto, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
